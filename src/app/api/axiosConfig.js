72ea0971089467db537cf4b538db1d08f4c38d8c
dedstinguerdsds <dedstinguerdsds@gmail.com>
2024-11-01 16:09:24 -0400

    cambio de ep backend

--- src/app/api/axiosConfig.js
+++ src/app/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const createAxios = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://pattient-api.onrender.com",
 });
 createAxios.interceptors.request.use((config) => {
   const token = localStorage.getItem("chiri_token");
